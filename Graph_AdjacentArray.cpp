ccda75a75dd6513cdc4ecfde102c6ea318f08730
237K <237ref@gmail.com>
2019-01-24 00:20:19 +0900

    [그래프] <1.인접행렬방식>  엑세스위반이 발생해서.. 5시간 째 원인 찾는 중..

--- Graph_AdjacentArray.cpp
+++ Graph_AdjacentArray.cpp
@@ -38,9 +38,8 @@
 
 #include <iostream>
 #include <fstream>
-#include <cstdio>
+#include <stdio.h>
 #include <vector>
-#include <string>
 using namespace std;
 
 class Graph_Adjacent_Array
@@ -53,23 +52,30 @@ public:
 	//생성자
 	explicit Graph_Adjacent_Array(int _Node_Num, int _Edge_Num) : GAA_Node_Num(_Node_Num), GAA_Edge_Num(_Edge_Num)
 	{
+		/*
 		try
 		{
 			if (_Node_Num <= 0 || _Edge_Num <= 0)					//Exception Handling
 				throw pair<int, int>(_Node_Num, _Edge_Num);
-
-			GAA_Array = new int *[_Node_Num];						//행 동적 할당
-			for (int i = 0; i < _Node_Num; ++i)
+				*/
+			GAA_Array = new int* [_Node_Num];						//행 동적 할당
+			for (int i = 1; i <= _Node_Num; ++i)
 			{
 				GAA_Array[i] = new int[_Node_Num];					//각 행 마다 열 동적 할당
-				memset(GAA_Array[i], 0, sizeof(int)*_Node_Num);		//배열 초기화
+
+				for (int j = 1; j <= _Node_Num; ++j)
+				{
+					GAA_Array[i][j] = 0;		//배열 초기화
+				}
 			}
+		/*
 		}
 		catch (pair<int, int> expn)
 		{
 			cout << "(error) graph 생성 실패" << endl;
 			cout << "error message : 입력받은 노드 수 : " << expn.first << " / 입력받은 간선 수 : " << expn.second << endl;
 		}
+		*/
 	}
 	
 	//소멸자
@@ -85,19 +91,21 @@ public:
 	//Edge(간선) 추가
 	void AddEdge(int Start, int End)
 	{
-		try
-		{
-			if (Start <= 0 || End <= 0)								//Exception Handling
-				throw Start;
+		//try
+		//{
+			//if (Start <= 0 || End <= 0)								//Exception Handling
+				//throw Start;
 
 			GAA_Array[Start][End] = 1;
 			GAA_Array[End][Start] = 1;
-		}
+		//}
+		/*
 		catch (int expn)
 		{
 			cout << "(error) 간선 추가 실패" << endl;
-			cout << "error message : 출발 노드 혹은 타겟 노드 값이 0이하 입니다." << endl;
+			cout << "error message("<<expn<<") : 출발 노드 혹은 타겟 노드 값이 0이하 입니다." << endl;
 		}
+		*/
 	}
 	void ViewAdjacentNode(int _Node) const
 	{
@@ -139,25 +147,45 @@ int main(void)
 	int test_case = 1;
 	int NumberOfNode = 1;
 	int NumberOfEdge = 1;
-	int start_buffer = 0, end_buffer = 0;
+	int start_buffer = 1, end_buffer = 1;
 
-	ifstream in("TestCase_graph1");
-	scanf("%d", &test_case);
-	
+	/*
+	ifstream in("TestCase_graph1.txt");
+
+	if (!in.is_open())
+		cout << "파일을 찾을 수 없습니다." << endl;
+		*/
+	//scanf("%d", &test_case);
+	//in >> test_case;
+	cout << "Test Case 개수 : ";
+	cin >> test_case;
+	Graph_Adjacent_Array *Graph[5];
 	for (int t = 1; t <= test_case; ++t)
 	{
-		scanf("%d %d", &NumberOfNode, &NumberOfEdge);
-		Graph_Adjacent_Array *Graph = new Graph_Adjacent_Array(NumberOfNode, NumberOfEdge);				//graph 객체 생성
+		//scanf("%d %d", &NumberOfNode, &NumberOfEdge);
+		//in >> NumberOfNode >> NumberOfEdge;
+		cout << "노드 개수, 간선 개수 : ";
+		cin >> NumberOfNode >> NumberOfEdge;
+		Graph[t] = new Graph_Adjacent_Array(NumberOfNode, NumberOfEdge);				//graph 객체 생성
 		for (int e = 1; e <= NumberOfEdge; ++e)
 		{
-			scanf("%d %d", &start_buffer, &end_buffer);
-			Graph->AddEdge(start_buffer, end_buffer);
+			//scanf("%d %d", &start_buffer, &end_buffer);
+			//in >> start_buffer >> end_buffer;
+			cout << "시작 노드, 타겟 노드 : ";
+			cin >> start_buffer >> end_buffer;
+			Graph[t]->AddEdge(start_buffer, end_buffer);
 		}
-
 		cout << "#" << t << endl;
-		Graph->ViewGAA;
-		Graph->ViewAdjacentNode;
-		delete Graph;
+		Graph[t]->ViewGAA();
+		Graph[t]->ViewAdjacentNode();
 	}
+
+	for (int t = 1; t <= test_case; ++t)
+	{
+		delete Graph[t];
+	}
+
+	//in.close();
+
 	return 0;
 }
