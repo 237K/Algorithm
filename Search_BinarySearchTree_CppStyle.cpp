cfe7db26a8c7ad245023f4a64f338572d22bc6c8
237K <237ref@gmail.com>
2019-03-10 17:49:21 +0900

    [Search] <3.이진탐색트리> 삭제 구현

--- Search_BinarySearchTree_CppStyle.cpp
+++ Search_BinarySearchTree_CppStyle.cpp
@@ -172,6 +172,7 @@ public:
 	T BSTGetNodeData();
 	void BSTInsert(BinaryTree<T> **pRoot, T data);
 	BinaryTree<T>* BSTSearch(BinaryTree<T> *bst, T target);
+	void BSTShowAll(BinaryTree<T> *bst);
 /////////////Binary Search Tree/////////////////////////////////////////////////////////////////////////////////////////////
 
 /////////////Delete/////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -179,6 +180,8 @@ public:
 	BinaryTree<T>* RemoveRightSubTree();
 	void ChangeLeftSubTree(BinaryTree<T>* main, BinaryTree<T>* sub);
 	void ChangeRightSubTree(BinaryTree<T>* main, BinaryTree<T>* sub);
+
+	BinaryTree<T>* BSTRemove(BinaryTree<T> **pRoot, T target);
 /////////////Delete/////////////////////////////////////////////////////////////////////////////////////////////////////////
 };
 
@@ -241,18 +244,40 @@ BinaryTree<T>* BinaryTree<T>::BSTSearch(BinaryTree<T> *bst, T target)
 	}
 	return NULL;
 }
+void ShowIntData(int data)
+{
+	cout << data << ' ';
+}
+
+template <typename T>
+void BinaryTree<T>::BSTShowAll(BinaryTree<T> *bst)
+{
+	InorderTraverse(bst, ShowIntData);
+}
 /////////////Binary Search Tree/////////////////////////////////////////////////////////////////////////////////////////////
 
 /////////////Delete/////////////////////////////////////////////////////////////////////////////////////////////////////////
 template <typename T>
 BinaryTree<T>* BinaryTree<T>::RemoveLeftSubTree()
 {
-
+	BinaryTree<T> *delNode = new BinaryTree<T>();
+	if (BTnode != NULL)
+	{
+		delNode = BTnode->left;
+		BTnode->left = NULL;
+	}
+	return delNode;
 }
 template <typename T>
 BinaryTree<T>* BinaryTree<T>::RemoveRightSubTree()
 {
-
+	BinaryTree<T> *delNode = new BinaryTree<T>();
+	if (BTnode != NULL)
+	{
+		delNode = BTnode->right;
+		BTnode->right = NULL;
+	}
+	return delNode;
 }
 template <typename T>
 void BinaryTree<T>::ChangeLeftSubTree(BinaryTree<T>* main, BinaryTree<T>* sub)
@@ -264,16 +289,104 @@ void BinaryTree<T>::ChangeRightSubTree(BinaryTree<T>* main, BinaryTree<T>* sub)
 {
 	main->BTnode->right = sub;
 }
-/////////////Delete/////////////////////////////////////////////////////////////////////////////////////////////////////////
-void ShowIntData(int data)
+template <typename T>
+BinaryTree<T>* BinaryTree<T>::BSTRemove(BinaryTree<T> **pRoot, T target)
 {
-	cout << data << ' ';
+	BinaryTree<T> *VirtualRootNode = new BinaryTree<T>();		//삭제 대상이 루트 노드인 경우를 고려하여 가상루트노드 변수 생성
+	BinaryTree<T> *parentNode = new BinaryTree<T>();
+	parentNode = VirtualRootNode;
+	BinaryTree<T> *currentNode = new BinaryTree<T>();
+	currentNode = *pRoot;
+	BinaryTree<T> *delNode = new BinaryTree<T>();
+
+	VirtualRootNode->ChangeRightSubTree(VirtualRootNode, *pRoot);
+
+	//Search target
+	while (currentNode != NULL && currentNode->GetData() != target)
+	{
+		parentNode = currentNode;
+		
+		if (currentNode->GetData() > target)
+			currentNode = currentNode->BTnode->left;
+		else
+			currentNode = currentNode->BTnode->right;
+	}
+	if (currentNode == NULL)
+		return NULL;
+
+	delNode = currentNode;
+
+	//Case 1) 삭제할 노드가 단말노드인 경우
+	if (delNode->BTnode->left == NULL && delNode->BTnode->right == NULL)
+	{
+		if (parentNode->BTnode->left == delNode)
+			parentNode->RemoveLeftSubTree();
+		else
+			parentNode->RemoveRightSubTree();
+	}
+
+	//Case 2) 삭제할 노드가 하나의 자식노드를 갖는 경우
+	else if (delNode->BTnode->left == NULL || delNode->BTnode->right == NULL)
+	{
+		BinaryTree<T> *childNode = new BinaryTree<T>();
+
+		if (delNode->BTnode->left != NULL)
+			childNode = delNode->BTnode->left;
+		else
+			childNode = delNode->BTnode->right;
+
+		if (parentNode->BTnode->left == delNode)
+			parentNode->ChangeLeftSubTree(parentNode, childNode);
+		else
+			parentNode->ChangeRightSubTree(parentNode, childNode);
+	}
+
+	//Case 3) 삭제할 노드가 두 개의 자식노드를 갖는 경우
+	else
+	{
+		BinaryTree<T> *replaceNode = new BinaryTree<T>();
+		replaceNode = delNode->BTnode->right;
+		BinaryTree<T> *replaceParentNode = new BinaryTree<T>();
+		replaceParentNode = delNode;
+
+		int delData;
+
+		//삭제할 노드의 대체 노드 검색
+		while (replaceNode->BTnode->left != NULL)
+		{
+			replaceParentNode = replaceNode;
+			replaceNode = replaceNode->BTnode->left;
+		}
+
+		//대체할 노드에 저장된 값을 삭제할 노드에 대입
+		delData = delNode->GetData();
+		delNode->SetData(replaceNode->GetData());
+
+		//대체할 노드의 부모노드와 자식노드를 연결
+		if (replaceParentNode->BTnode->left == replaceNode)
+			replaceParentNode->ChangeLeftSubTree(replaceParentNode, replaceNode->BTnode->right);
+		else
+			replaceParentNode->ChangeRightSubTree(replaceParentNode, replaceNode->BTnode->right);
+
+		delNode = replaceNode;
+		delNode->SetData(delData);
+	}
+
+	//삭제된 노드가 루트노드인 경우
+	if (VirtualRootNode->BTnode->right != *pRoot)
+		*pRoot = VirtualRootNode->BTnode->right;
+
+	//delete VirtualRootNode;
+	//VirtualRootNode = NULL;
+	return delNode;
 }
+/////////////Delete/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main(void)
 {
 	BinaryTree<int> *bstRoot = new BinaryTree<int>();
 	BinaryTree<int> *searchNode = new BinaryTree<int>();
+	BinaryTree<int> *deleteNode = new BinaryTree<int>();
 
 	bstRoot->BSTInsert(&bstRoot, 7);
 	bstRoot->BSTInsert(&bstRoot, 1);
@@ -284,6 +397,10 @@ int main(void)
 	bstRoot->BSTInsert(&bstRoot, 2);
 	bstRoot->BSTInsert(&bstRoot, 3);
 	bstRoot->BSTInsert(&bstRoot, 6);
+
+	cout << "Current Binary Tree : ";
+	bstRoot->BSTShowAll(bstRoot);
+	cout << endl<<endl;
 
 	cout << "Search 1" << endl;
 	searchNode = bstRoot->BSTSearch(bstRoot, 1);
@@ -319,6 +436,13 @@ int main(void)
 		cout << "Fail" << endl << endl;
 	else
 		cout << "Key : " << searchNode->BSTGetNodeData() << endl << endl;
+
+	cout << "Delete 3" << endl;
+	deleteNode = bstRoot->BSTRemove(&bstRoot, 3);
+	cout << "Deleted : " << deleteNode->BSTGetNodeData() << endl;
+	cout << "Current Binary Tree : ";
+	bstRoot->BSTShowAll(bstRoot);
+	cout << endl << endl;
 
 	return 0;
 }
